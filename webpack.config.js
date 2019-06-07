9316b983e0e3521c16f1ae576f4c9526dd248519
Corey Lantz <corey.lantz12@gmail.com>
2019-06-06 23:08:21 -0400

    add scss loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,19 @@ module.exports = {
     app: './src/index.js',
   },
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
   }
 };
